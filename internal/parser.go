387c82123f82c38e5d9352338e098f691324a62d
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2026-09-21 22:08:33 +0900

    server

--- internal/parser.go
+++ internal/parser.go
@@ -3,6 +3,8 @@ package internal
 import (
 	"flag"
 	"log/slog"
+	"os"
+	"path/filepath"
 )
 
 type args struct {
@@ -18,10 +20,22 @@ func (a args) LogLevel() slog.Level {
 	return level
 }
 
+func defaultConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, ".agentpet", "config.toml"), nil
+}
+
 func ParseArgs() (args, error) {
+	defaultConfig, err := defaultConfigPath()
+	if err != nil {
+		return args{}, err
+	}
 	var configFile string
 	var verbose bool
-	flag.StringVar(&configFile, "c", "", "path to config file (TOML)")
+	flag.StringVar(&configFile, "c", defaultConfig, "path to config file (TOML)")
 	flag.BoolVar(&verbose, "v", false, "enable verbose logging")
 	flag.Parse()
 	return args{ConfigFile: configFile, verbose: verbose}, nil
--- internal/parser_test.go
+++ internal/parser_test.go
@@ -2,8 +2,36 @@ package internal
 
 import (
 	"log/slog"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+func TestDefaultConfigPath(t *testing.T) {
+	t.Parallel()
+	home, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cases := []struct {
+		name string
+		want string
+	}{
+		{"returns a path rooted at the user home directory", filepath.Join(home, ".agentpet", "config.toml")},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := defaultConfigPath()
+			if err != nil {
+				t.Fatalf("defaultConfigPath() error = %v", err)
+			}
+			if got != c.want {
+				t.Errorf("got %q, want %q", got, c.want)
+			}
+		})
+	}
+}
 
 func TestLogLevel(t *testing.T) {
 	t.Parallel()
